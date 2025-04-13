084488d55feb3f7092d422f22f3c23595802a56d
jadghallali <2006092400122@ofppt-edu.ma>
2025-04-13 19:15:25 +0100

    Add files via upload

---
+++ devine.js
@@ -0,0 +1,21 @@
+let nbAleatoire = Math.floor(Math.random() * 10)
+console.log(nbAleatoire)
+let cpt = 0
+function devine() {
+    cpt++
+    var nbUser = document.getElementById("dv1").value;
+    if (cpt < 3) {
+        if (nbAleatoire == nbUser) {
+            document.getElementById("resultat").innerHTML = "Success";
+            document.getElementById("btn").disabled = true
+        } else if (nbAleatoire > nbUser) {
+            document.getElementById("resultat").innerHTML = "Le nombre est plus grand";
+        } else {
+            document.getElementById("resultat").innerHTML = "Le nombre est plus petit";
+        }
+    } else {
+        document.getElementById("resultat").innerHTML = "game over"
+        document.getElementById("btn").disabled = true
+    }
+}
+
